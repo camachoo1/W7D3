52ff78e6264022fcd834d29bd99695437cf8fe46
“Omar <ocamacho812@gmail.com>
2023-01-18 15:10:25 -0800

    added bands w/ controller and views

--- music_app/app/controllers/bands_controller.rb
+++ music_app/app/controllers/bands_controller.rb
@@ -4,13 +4,13 @@ class BandsController < ApplicationController
   def set_band
     @band = Band.find_by(id: params[:id])
   end
+
   def index
     @bands = Band.all
   end
 
   def create
-    @band = Band.new
-
+    @band = Band.new(band_params)
     if @band.save
       redirect_to band_url(@band)
     else
@@ -23,15 +23,15 @@ class BandsController < ApplicationController
   end
 
   def edit
-    @band = Band.find_by(id: params[:id])
+    # @band = Band.find_by(id: params[:id])
   end
 
   def show
-    @band = Band.find_by(id: params[:id])
+    # @band = Band.find_by(id: params[:id])
   end
 
   def update
-    @band = Band.find_by(id: params[:id])
+    # @band = Band.find_by(id: params[:id])
 
     if @band.update(band_params)
       redirect_to band_url(@band)
@@ -41,7 +41,7 @@ class BandsController < ApplicationController
   end
 
   def destroy
-    @band = Band.find_by(id: params[:id])
+    # @band = Band.find_by(id: params[:id])
 
     if @band.destroy
       redirect_to bands_url
